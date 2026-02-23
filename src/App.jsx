6f20f1e7739ab18b00abb97006b548e689fed99c
Wes Lovick <16548399+wlovick@users.noreply.github.com>
2026-02-22 20:38:53 -0600

    fixed initial load

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+import Home from "./pages/Login";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Todos from "./pages/Todos";
